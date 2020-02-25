8f3258fd198fe943284a07ea371025b4f47461fd
Tala99 <hethnawi.tala@gmail.com>
2020-02-25 15:45:35 +0200

    new homework sw

---
+++ src/software/Test1.java
@@ -0,0 +1,13 @@
+package software;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+public class Test1 {
+    @Test
+    public void tt(){
+        assertEquals(Calculater.add(4,5), 6);
+    }   
+}
+
+
